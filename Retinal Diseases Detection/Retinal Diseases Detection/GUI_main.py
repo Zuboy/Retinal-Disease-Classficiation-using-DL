1de4e78204d08e78c4db38493269a010eacdef9e
Aishwaryaraj Chandel <aishwaryaraj.chandel@gmail.com>
2024-11-18 18:16:09 +0530

    Update GUI_main.py

--- Retinal Diseases Detection/Retinal Diseases Detection/GUI_main.py
+++ Retinal Diseases Detection/Retinal Diseases Detection/GUI_main.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 29 14:27:05 2021
-
-@author: om
+Created on Jan 29 2024 , Aishwaryaraj
 """
 
 import tkinter as tk
@@ -15,28 +13,21 @@ import sqlite3
 import os
 import numpy as np
 import time
-#import video_capture as value
-#import lecture_details as detail_data
-#import video_second as video1
 
-#import lecture_video  as video
 
 global fn
 fn = ""
 ##############################################+=============================================================
 root = tk.Tk()
 root.configure(background="brown")
-# root.geometry("1300x700")
+
 
 
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 root.geometry("%dx%d+0+0" % (w, h))
 root.title("Retinal disease classification using deep learning")
 
-# 43
 
-# ++++++++++++++++++++++++++++++++++++++++++++
-#####For background Image
 image2 = Image.open('2.jpg')
 image2 = image2.resize((1800, 850))
 
@@ -46,31 +37,12 @@ background_label = tk.Label(root, image=background_image)
 
 background_label.image = background_image
 
-background_label.place(x=0, y=0)  # , relwidth=1, relheight=1)
+background_label.place(x=0, y=0)  
 #
 label_l1 = tk.Label(root, text="Retinal disease classification using deep learning",font=("Times New Roman", 30, 'bold'),
                     background="brown", fg="white", width=70, height=1)
 label_l1.place(x=0, y=0)
 
-#T1.tag_configure("center", justify='center')
-#T1.tag_add("center", 1.0, "end")
-
-################################$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#def clear_img():
-#    img11 = tk.Label(root, background='bisque2')
-#    img11.place(x=0, y=0)
-
-
-#################################################################$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-################################$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# def cap_video():
-    
-#     video1.upload()
-#     #from subprocess import call
-#     #call(['python','video_second.py'])
 
 def reg():
     from subprocess import call
